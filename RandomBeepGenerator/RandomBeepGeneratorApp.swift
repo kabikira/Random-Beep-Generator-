8ec44dc816bfb72e7e7d820d75370e0d1ca15049
kabikira <kabikirar2@protonmail.com>
2022-12-17 21:54:48 +0900

    Initial Commit

---
+++ RandomBeepGenerator/RandomBeepGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomBeepGeneratorApp.swift
+//  RandomBeepGenerator
+//
+//  Created by koala panda on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct RandomBeepGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
