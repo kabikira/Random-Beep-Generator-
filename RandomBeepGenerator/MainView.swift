f38979080a74dec9e29f47e3b08280387ef1f40c
kabikira <kabikirar2@protonmail.com>
2022-12-18 18:50:52 +0900

    add Button

--- RandomBeepGenerator/MainView.swift
+++ RandomBeepGenerator/MainView.swift
@@ -15,27 +15,55 @@ class NoiseConductor {
         engine.output = noise
         try! engine.start()
         noise.play()
-        while true {
-            noise.amplitude = Float.random(in: -1 ... 1)
-            usleep(100_000)
+        let timer = Timer
+            .scheduledTimer(
+                withTimeInterval: 0.1,
+                repeats: true
+            ) { _ in
+                print("NoisePlay")
+                self.noise.amplitude = Float.random(in: -1 ... 1)
+            }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+            timer.invalidate()
+            self.noise.stop()
+            print("NoiseStop")
         }
+    
+    }
+    func stopNoise() {
+        noise.stop()
     }
 }
 
 class OscillatorConductor {
     let osc = PlaygroundOscillator()
     let engine = AudioEngine()
+    var stopTime: Double = 10
     
     func playOsc() {
         engine.output = osc
         try! engine.start()
         osc.play()
-        while true {
-            osc.frequency = Float.random(in: 200 ... 2200)
-            osc.amplitude = Float.random(in: 0.0 ... 0.3)
-            usleep(100_000)
+        let timer = Timer
+            .scheduledTimer(
+                withTimeInterval: 0.1, repeats: true
+            ) { _ in
+                print("Oscplay")
+                self.osc.frequency = Float.random(in: 200 ... 800)
+                self.osc.amplitude = Float.random(in: 0 ... 0.3)
+            }
+        DispatchQueue.main.asyncAfter(deadline: .now() + self.stopTime) {
+            timer.invalidate()
+            self.osc.stop()
+            print("OscStop")
         }
+        
     }
+    func stopOsc() {
+        osc.stop()
+    }
+  
 }
 
 
@@ -49,6 +77,12 @@ struct MainView: View {
             }
             Button("NoisePlay") {
                 noise.playNoise()
+            }
+            Button("OscStop") {
+                oscillator.stopOsc()
+            }
+            Button("NoiseStop") {
+                noise.stopNoise()
             }
         }
     }
